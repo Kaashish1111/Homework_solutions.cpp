6b84550d1ce1b3a5ab10b2a2ba1fa18132f2be4a
Kashish Goyal <kashish1782004@gmail.com>
2024-09-05 17:02:27 +0530

    day 29 completed

---
+++ day29_solutions/q2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    for (int i = 1; i< n; i++) {
+        int j = i;
+        while (j > 0 && arr[j] > arr[j - 1]) {
+            std::swap(arr[j], arr[j - 1]);
+            j--;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
